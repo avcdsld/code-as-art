28f44930bfeaa560c7d09c135d5fe467103fb231
avcdsld <avcdsld@gmail.com>
2024-03-02 04:38:15 +0900

    peace
    https://twitter.com/arandoros/status/1742029170115518748

---
+++ code-poetry/ruby/peace.rb
@@ -0,0 +1,27 @@
+class World
+  def initialize
+    @peace = true
+    Thread.new { run_world_cycle }　
+    Thread.new { recover_peace }
+  end
+
+  private
+
+  def run_world_cycle
+    loop do
+      if rand < WHISPER_OF_CHAOS
+        @peace = false
+      end
+      sleep MOMENT_OF_REFLECTION
+    end
+  end
+
+  def recover_peace
+    loop do
+      sleep BREATH_OF_HARMONY
+      @peace = true
+    end
+  end
+end
+  
+earth = World.new
